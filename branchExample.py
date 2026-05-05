457508b31d698216989f6645f8548ee020314b83
iDxtry <102931413+iDxtry@users.noreply.github.com>
2026-05-05 09:18:22 -0400

    Create branchExample.py
    1. Created a python script to demo Github branches.

---
+++ branchExample.py
@@ -0,0 +1,12 @@
+"""
+Name: branchExample.py
+Coded by: A. Batra and E. Yip
+Purpose: Demonstrate how to use Github branches
+Date: 5/5/26
+"""
+
+counter = 0
+
+while(counter < 10):
+    print("Time: " + str(counter))
+    counter += 1
