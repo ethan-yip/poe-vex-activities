d537065ce43bdb8ba7d17e8bed6aee454d32bcb9
iDxtry <102931413+iDxtry@users.noreply.github.com>
2026-05-05 09:45:57 -0400

    Created VEX project for RECbot lessons

---
+++ batra-vex/src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       Anish Batra and Ethan Yip                                                   #
+# 	Created:      5/5/2026, 9:42:24 AM                                         #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
